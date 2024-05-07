27c12a89ce6f0e207087f1732f3a6431f674d5e8
kimh7537 <kimh7537@gmail.com>
2024-05-07 19:19:29 +0900

    feat: signin, signup 기능 구현

--- Database/pythonProject/app.py
+++ Database/pythonProject/app.py
@@ -7,12 +7,31 @@ cur = connect.cursor()
 
 @app.route('/')
 def main():
-    return render_template("main.html")
+    return render_template("login.html")
 
 @app.route('/register', methods=['POST'])
 def register():
     id = request.form['id']
     password = request.form['password']
+    send = request.form["send"]
+
+    if send == "sign up":
+        cur.execute("SELECT id FROM users WHERE id = '{}';".format(id))
+        result = cur.fetchone()
+        if result or id == "":
+            return render_template("signup_fail.html")
+        elif not result and id != "":
+            cur.execute("INSERT INTO users VALUES('{}', '{}', '{}');".format(id, password, 'user'))
+            connect.commit()
+            return render_template("main.html")
+    elif send == "sign in":
+        cur.execute("SELECT id, password FROM users WHERE id = '{}' and password='{}';".format(id, password))
+        result = cur.fetchone()
+        if not result:
+            return render_template("signin_fail.html")
+        elif result[0] == id and result[1] == password:
+            return render_template("main.html")
+
 
 
 
