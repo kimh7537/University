8a6fd84e3e8487e5544382b5994bf5f8e6d8c046
kimh7537 <kimh7537@gmail.com>
2024-05-10 13:42:31 +0900

    feat: movie_info 구현 마무리

--- Database/pythonProject/app.py
+++ Database/pythonProject/app.py
@@ -1,5 +1,5 @@
 import psycopg2
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 connect = psycopg2.connect("dbname=term24 user=postgres password=kj003852@")
@@ -77,24 +77,114 @@ def review_sort():
     return render_template("main.html", reviews=reviews, movies=movies, user = id)
 
 
-@app.route('/movie_info', methods=['POST'])
+@app.route('/movie_info', methods=['POST', 'GET'])
 def movie_info():
-    id = request.form["id"]
-    movie = request.form["movie"]
-    cur.execute("SELECT title, director, genre, rel_date FROM movies WHERE title = '{}';".format(movie))
+    id = request.form.get("id") or request.args.get("id")
+    movie = request.form.get("movie") or request.args.get("movie") #movie name
+    cur.execute("SELECT * FROM movies WHERE title = '{}';".format(movie))
     movie_info = cur.fetchone()
 
-    # cur.execute("SELECT title, round(avg(ratings),1), director, genre, rel_date FROM movies LEFT JOIN reviews ON movies.id = reviews.mid GROUP BY title, director, genre, rel_date ORDER BY rel_date DESC;")
-    # movies = cur.fetchall();
-    #
-    # if send == "latest":
-    #     cur.execute("SELECT ratings, uid, title, review, TO_CHAR(rev_time, 'YYYY-MM-DD HH24:MI:SS') FROM reviews JOIN movies ON movies.id = reviews.mid ORDER BY rev_time DESC;")
-    #     reviews = cur.fetchall();
-    # elif send == "title":
-    #     cur.execute("SELECT ratings, uid, title, review, TO_CHAR(rev_time, 'YYYY-MM-DD HH24:MI:SS') FROM reviews JOIN movies ON movies.id = reviews.mid ORDER BY title;")
-    #     reviews = cur.fetchall();
+    cur.execute("""
+        SELECT ratings, uid, review, TO_CHAR(rev_time, 'YYYY-MM-DD HH24:MI:SS')
+        FROM reviews
+        WHERE mid = '{}'
+        AND uid NOT IN (SELECT opid FROM ties WHERE id = '{}' AND tie = 'mute');
+        """.format(movie_info[0], id))
+    reviews = cur.fetchall()
 
-    return render_template("movie_info.html", movie_info=movie_info, user = id)
+    cur.execute("SELECT ROUND(AVG(ratings), 1) FROM reviews WHERE mid = '{}';".format(movie_info[0]))
+    average_rating = cur.fetchone()[0]
+
+    return render_template("movie_info.html", movie_info=movie_info, reviews=reviews, average_rating=average_rating, user = id)
+
+
+
+@app.route('/submit_review', methods=['POST'])
+def submit_review():
+    user_id = request.form['id']
+    movie_id = request.form['movie']
+    movie_name = request.form['movie_name']
+    ratings = int(request.form['ratings'])
+    review_text = request.form['review_text']
+
+    # Check if the user has already reviewed this movie, and update if necessary
+    cur.execute("SELECT * FROM reviews WHERE uid = '{}' AND mid = '{}';".format(user_id, movie_id))
+    existing_review = cur.fetchone()
+
+    if existing_review:
+        cur.execute("""
+            UPDATE reviews
+            SET ratings = '{}', review = '{}', rev_time = CURRENT_TIMESTAMP
+            WHERE uid = '{}' AND mid = '{}';
+        """.format(ratings, review_text, user_id, movie_id))
+    else:
+        cur.execute("""
+            INSERT INTO reviews (mid, uid, title, ratings, review, rev_time)
+            VALUES ('{}', '{}', '{}', '{}', CURRENT_TIMESTAMP);
+        """.format(movie_id, user_id, ratings, review_text))
+
+    connect.commit()
+
+    return redirect(url_for('movie_info', id=user_id, movie=movie_name))
+
+
+
+@app.route('/user_info', methods=['POST', 'GET'])
+def user_info():
+    my_id = request.form.get("id") or request.args.get("id")  # The ID of the logged-in user
+    target_user_id = request.form.get("review") or request.args.get("user_id")  # The ID of the user being viewed
+
+    if not my_id or not target_user_id:
+        return "My ID and Target User ID are required.", 400
+
+    # Fetch reviews written by the target user
+    cur.execute("""
+        SELECT ratings, title, review, TO_CHAR(rev_time, 'YYYY-MM-DD HH24:MI:SS') AS formatted_time
+        FROM reviews
+        WHERE uid = %s
+        ORDER BY rev_time DESC;
+    """, (target_user_id,))
+    reviews = cur.fetchall()
+
+    # Determine the followers and people the user follows/mutes
+    cur.execute("SELECT id FROM ties WHERE opid = %s AND tie = 'follow';", (target_user_id,))
+    followers = cur.fetchall()
+
+    cur.execute("SELECT opid FROM ties WHERE id = %s AND tie = 'follow';", (target_user_id,))
+    followed_users = cur.fetchall()
+
+    cur.execute("SELECT opid FROM ties WHERE id = %s AND tie = 'mute';", (target_user_id,))
+    muted_users = cur.fetchall()
+
+    # If the target user is an admin, skip follow/mute controls
+    cur.execute("SELECT role FROM users WHERE id = %s;", (target_user_id,))
+    role = cur.fetchone()[0]
+
+    return render_template(
+        "user_info.html",
+        user=my_id,
+        target_user_id=target_user_id,
+        reviews=reviews,
+        followers=followers,
+        followed_users=followed_users,
+        muted_users=muted_users,
+        role=role
+    )
+
+
+
+
+
+@app.route('/main_page', methods=['POST', 'GET'])
+def main_page():
+    id = request.args["id"]
+
+    cur.execute("SELECT title, round(avg(ratings),1), director, genre, rel_date FROM movies LEFT JOIN reviews ON movies.id = reviews.mid GROUP BY title, director, genre, rel_date ORDER BY rel_date DESC;")
+    movies = cur.fetchall();
+    cur.execute("SELECT ratings, uid, title, review, TO_CHAR(rev_time, 'YYYY-MM-DD HH24:MI:SS') FROM reviews JOIN movies ON movies.id = reviews.mid ORDER BY rev_time DESC;")
+    reviews = cur.fetchall();
+
+    return render_template("main.html", reviews = reviews, movies=movies, user = id)
 
 
 if __name__ == '__main__':
